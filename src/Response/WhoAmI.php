778fd32e10394c02b79b78b9c4648d2b8bf0253d
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-02-04 17:10:49 +1100

    Add remaining whoami field getters

--- src/Response/WhoAmI.php
+++ src/Response/WhoAmI.php
@@ -9,7 +9,19 @@ class WhoAmI {
     $this->data = json_decode($response->getBody()->getContents());
   }
 
+  public function clientId() {
+    return $this->data->clientId;
+  }
+
   public function userId() {
     return $this->data->userId;
+  }
+
+  public function scopes() {
+    return $this->data->scopes;
+  }
+
+  public function ttl() {
+    return $this->data->ttl;
   }
 }
