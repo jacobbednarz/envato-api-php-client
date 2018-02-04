68a1915c90787b07402f5f2e457049fa70f21109
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-02-05 08:48:10 +1100

    Add test coverage for ApiClient

---
+++ tests/ApiClientTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use Envato\ApiClient;
+use PHPUnit\Framework\TestCase;
+
+final class ApiClientTest extends TestCase {
+  public function setUp() {
+    $this->client = ApiClient::factory(array(
+      'token' => 'notarealtoken'
+    ));
+  }
+
+  public function testClientApiTokenIsRequired() {
+    $this->expectException(Envato\Exceptions\MissingClientTokenException::class);
+    ApiClient::factory(array());
+  }
+
+  public function testClientHasRequestTimeout() {
+    $this->assertArrayHasKey('timeout', $this->client->getConfig());
+  }
+
+  public function testClientHasConnectTimeout() {
+    $this->assertArrayHasKey('connect_timeout', $this->client->getConfig());
+  }
+
+  public function testClientVerifiesConnection() {
+    $this->assertEquals($this->client->getConfig()['verify'], true);
+  }
+
+  public function testClientDoesNotFollowRedirects() {
+    $this->assertEquals($this->client->getConfig()['allow_redirects'], false);
+  }
+
+  public function testClientHasUserAgent() {
+    $this->assertContains(
+      'Envato PHP SDK/',
+      $this->client->getConfig()['headers']['User-Agent']
+    );
+  }
+
+  public function testClientAllowsOverriddingExistingConfigurationKeys() {
+    $local_client = ApiClient::factory(array(
+      'timeout' => '999',
+      'token' => 'anotherkey',
+    ));
+
+    $this->assertEquals($local_client->getConfig()['timeout'], '999');
+  }
+
+  public function testClientAllowsOverriddingExistingHeaders() {
+    $local_client = ApiClient::factory(array(
+      'token' => 'anotherkey',
+      'headers' => array(
+        'User-Agent' => 'Super Duper Client',
+      ),
+    ));
+
+    $this->assertEquals(
+      $local_client->getConfig()['headers']['User-Agent'],
+      'Super Duper Client'
+    );
+  }
+}
