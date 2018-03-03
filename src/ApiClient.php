45b43aaabe8e50989355373f523ddb308bd197d9
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-03-03 12:28:41 +1100

    Hook up the `RateLimiter` into the `ApiClient`
    Updates the `ApiClient` to insert the middleware (for the request and
    response) and throw the appropriate exceptions as needed.

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -3,6 +3,12 @@
 namespace Envato;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Handler\CurlHandler;
+use Envato\Middleware\RateLimiter;
 
 class ApiClient extends Client {
   const VERSION    = '0.1';
@@ -11,10 +17,40 @@ class ApiClient extends Client {
 
   public static function factory($config = array()) {
     if (empty($config['token'])) {
-      throw new Exception\MissingClientTokenException('Missing required API token.');
+      throw new Exception\MissingClientTokenException;
     }
 
+    $rate_limiter = new RateLimiter();
+    $stack = new HandlerStack();
+    $stack->setHandler(new CurlHandler());
+
+    $stack->push(
+      Middleware::mapRequest(
+        function (RequestInterface $request) use ($rate_limiter) {
+          if ($rate_limiter->isThrottled()) {
+            throw new Exception\ClientIsRateLimitedException;
+          } else {
+            return $request;
+          }
+        }
+      )
+    );
+
+    $stack->push(
+      Middleware::mapResponse(
+        function (ResponseInterface $response) use ($rate_limiter) {
+          if ($response->getStatusCode() == 429) {
+            $rate_limiter->setThrottle($response->getHeader('Retry-After')[0]);
+            throw new Exception\ClientIsRateLimitedException;
+          } else {
+            return $response;
+          }
+        }
+      )
+    );
+
     $defaults = array(
+      'handler' => $stack,
       'base_uri' => self::BASE_URI,
       'connect_timeout' => '3',
       'timeout' => '10',
